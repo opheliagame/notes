6864a49206de4050c4245ffe6dea382756049eff
opheliagame <21999998+opheliagame@users.noreply.github.com>
2023-10-12 23:35:21 +0900

    added path prefix to 11ty config

--- .eleventy.js
+++ .eleventy.js
@@ -214,6 +214,7 @@ module.exports = function(eleventyConfig) {
         htmlTemplateEngine: "njk",
         markdownTemplateEngine: "njk",
         passthroughFileCopy: true,
+        pathPrefix: '/notes/',
     };
 
 };
